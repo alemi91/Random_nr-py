c8c9a344034b88ed5e85175770fab1f3e4e35b63
alemi91 <amihalache2006@gmail.com>
2026-06-20 12:54:49 +0300

    update code

--- random_nr.py
+++ random_nr.py
@@ -1,10 +1,35 @@
 import random
+import sys
 
-a = int(input("a = "))
-b = int(input("b = "))
+print("Guess the number ^_^")
+print("Give me a range [a, b] to choose from!")
+try:
+    a = int(input("a = "))
+    b = int(input("b = "))
+except ValueError:
+    print("Try again lul.")
+    sys.exit()
 
 if a > b:
-    print("Try again lul")
+    print("Try again lul.")
 else:
-    nr = random.randint(a, b)
-    print(f"Random number: {nr}")
+    secret = random.randint(a, b)
+    incercari = 0
+    guessed = False
+    
+    print(f"\nWhat number did I choose?=]")
+    
+    while not guessed:
+        try:
+            nr = int(input("Varianta ta: "))
+            incercari += 1
+            
+            if nr < secret:
+                print("Too smol :/")
+            elif nr > secret:
+                print("Too big :/")
+            else:
+                guessed = True
+                print(f"Correct! Took you {incercari} tries btw!")
+        except ValueError:
+            print("Invalid number :|")
